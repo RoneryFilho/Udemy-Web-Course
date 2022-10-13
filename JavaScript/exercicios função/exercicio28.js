e0ff43d3aad382f9d046543b2d37f335c0b2699e
Ronery Ruhmann Freitas Filho <roneryrffilho@gmail.com>
2022-10-13 14:03:48 -0300

    Create exercicio28.js

---
+++ JavaScript/exercicios função/exercicio28.js
@@ -0,0 +1,21 @@
+//le um array de inteiros e diz se são pares ou impares
+
+function parOuImpar(array){
+    
+    let impares = 0;
+    let pares = 0;
+    let i;
+    
+    for(i in array){
+        if((array[i] % 2) == 0){
+            pares ++;
+        }else{
+            impares++;
+        }
+    }
+    console.log(`ímpares: ${impares} Pares: ${pares}`);
+}
+
+num = [0, 1, 2, 3, 4,, 5, 6 ,7, 8, 9];
+
+parOuImpar(num);
